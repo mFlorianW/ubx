28b886f394ab54eb1b9c46dfae4aaf2c7d18b719
Florian Weßel <florianwessel@gmx.net>
2021-12-29 14:51:25 +0100

    Use sepeartor message reader for ack message.

--- include/messages/ubx_ack_message.hpp
+++ include/messages/ubx_ack_message.hpp
@@ -3,36 +3,52 @@
 
 #include "ubx_message.hpp"
 #include "ubx_ack_nack_message.hpp"
+#include "private/ubx_ack_nack_message_reader.hpp"
 #include <iterator>
 
 namespace ubx
 {
-class ack_message final : public message_base<ack_message>, public ubx_ack_nack_message
+class ack_message final : public message_base<ack_message>
 {
 public:
     ack_message() = default;
 
     template<typename read_iterator>
-    ack_message(const read_iterator &begin, const read_iterator &end)
-        : message_base<ack_message>()
-        , ubx_ack_nack_message(begin, end)
-    {
-        if(m_succesful_parsed)
-        {
-            m_status = true;
-        }
-    }
+    ack_message(const read_iterator &begin, const read_iterator &end);
 
-    std::uint8_t get_acknowledged_class_id() const noexcept
-    {
-        return m_class_id;
-    }
+    std::uint8_t get_acknowledged_class_id() const noexcept;
 
-    std::uint8_t get_acknowledged_message_id() const noexcept
-    {
-        return m_message_id;
-    }
+    std::uint8_t get_acknowledged_message_id() const noexcept;
+
+private:
+    std::uint8_t m_class_id{0};
+    std::uint8_t m_message_id{0};
 };
+
+template<typename read_iterator>
+ack_message::ack_message(const read_iterator &begin, const read_iterator &end)
+    : message_base<ack_message>()
+{
+    auto message_reader = ubx_ack_nack_message_reader();
+    if(!message_reader.read(begin, end))
+    {
+        return;
+    }
+
+    m_class_id = message_reader.get_class_id();
+    m_message_id = message_reader.get_message_id();
+    m_status = true;
+}
+
+inline std::uint8_t ack_message::get_acknowledged_class_id() const noexcept
+{
+    return m_class_id;
+}
+
+inline std::uint8_t ack_message::get_acknowledged_message_id() const noexcept
+{
+    return m_message_id;
+}
 
 } // namespace ubx
 
---
+++ include/private/ubx_ack_nack_message_reader.hpp
@@ -0,0 +1,57 @@
+#ifndef UBX_ACK_NACK_MESSAGE_READER_HPP
+#define UBX_ACK_NACK_MESSAGE_READER_HPP
+
+#include <cstdint>
+#include <iterator>
+
+namespace ubx
+{
+
+class ubx_ack_nack_message_reader
+{
+public:
+    ubx_ack_nack_message_reader() = default;
+
+    template<typename read_iterator>
+    bool read(const read_iterator &begin, const read_iterator &end);
+
+    std::uint8_t get_class_id() const;
+
+    std::uint8_t get_message_id() const;
+
+private:
+    std::uint8_t m_class_id{0};
+    std::uint8_t m_message_id{0};
+};
+
+template<typename read_iterator>
+bool ubx_ack_nack_message_reader::read(const read_iterator &begin, const read_iterator &end)
+{
+    static_assert(std::is_same<typename std::iterator_traits<read_iterator>::value_type,  std::uint8_t>::value,
+                  "The iterator must be of type std::unit8_t");
+
+    constexpr size_t msg_size = 2;
+    if(std::distance(begin, end) < msg_size)
+    {
+        return false;
+    }
+
+    m_class_id = begin[0];
+    m_message_id = begin[1];
+
+    return true;
+}
+
+inline std::uint8_t ubx_ack_nack_message_reader::get_class_id() const
+{
+    return m_class_id;
+}
+
+inline std::uint8_t ubx_ack_nack_message_reader::get_message_id() const
+{
+    return m_message_id;
+}
+
+} // namespace
+
+#endif // UBX_ACK_NACK_MESSAGE_READER_HPP
