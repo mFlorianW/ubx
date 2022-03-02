ed6e636be1b604d9b5089e047f32f78134e035aa
Florian Weßel <florianwessel@gmx.net>
2022-03-02 22:02:11 +0100

    Handle unsupported messages and move frame factor into the header

--- include/ubx_frame_processor.hpp
+++ include/ubx_frame_processor.hpp
@@ -2,13 +2,13 @@
 #define FRAME_PROCESSOR_H
 
 #include "ubx_frame.hpp"
-#include <tuple>
 #include <algorithm>
+#include <tuple>
 
 namespace ubx
 {
 
-template<typename message_factory_t, typename message_handler_t>
+template <typename message_factory_t, typename message_handler_t>
 class frame_processor
 {
 public:
@@ -25,18 +25,24 @@ public:
 
     virtual ~frame_processor() = default;
 
-    template<typename read_iterator>
+    template <typename read_iterator>
     void process_data(read_iterator data_begin, read_iterator data_end)
     {
         frame<read_iterator> fr;
-        while(data_begin != data_end)
+        while (data_begin != data_end)
         {
-            if(fr.read(data_begin, data_end) == frame_read_result::ok)
+            if (fr.read(data_begin, data_end) == frame_read_result::ok)
             {
                 auto msg_ptr = m_msg_factory.create_message(fr.get_class_id(),
                                                             fr.get_message_id(),
                                                             fr.get_payload_begin(),
                                                             fr.get_payload_end());
+                if (msg_ptr == nullptr)
+                {
+                    // printf("message pointer is nullptr \n");
+                    ++data_begin;
+                    continue;
+                }
                 msg_ptr->dispatch(m_msg_handler);
                 data_begin = fr.get_frame_end();
             }
@@ -52,6 +58,6 @@ private:
     message_handler_t &m_msg_handler;
 };
 
-} //namespace ubx
+} // namespace ubx
 
 #endif // FRAME_PROCESSOR_H
